fe9f7cf5e430b344389a8c521ecf1a6745a1fe3f
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2021-04-28 08:44:18 +0900

    first commit

---
+++ app/src/main/java/com/jp_funda/urlfolder/Activities/ui/home/HomeFragment.java
@@ -0,0 +1,26 @@
+package com.jp_funda.urlfolder.Activities.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.jp_funda.urlfolder.R;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return root;
+    }
+}
